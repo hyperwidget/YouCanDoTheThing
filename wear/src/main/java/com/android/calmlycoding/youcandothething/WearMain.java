4a81f6f966e83bd2e2947ec27f2195913996145a
Hunter Jansen <rawkamatic@gmail.com>
2014-07-02 21:41:14 -0400

    Update wear to not show ui - simply create the notification.
    More inline to original thoughts at the moment

--- wear/src/main/java/com/android/calmlycoding/youcandothething/WearMain.java
+++ wear/src/main/java/com/android/calmlycoding/youcandothething/WearMain.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
-import android.support.v4.app.NotificationCompat.WearableExtender;
 
 public class WearMain extends Activity {
 
@@ -16,7 +15,33 @@ public class WearMain extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wear_main);
 
+        notifyTest();
+    }
+
+    public void notifyTest(){
+        int notificationId = 001;
+// Build intent for notification content
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+
+        PendingIntent mapPendingIntent =
+                PendingIntent.getActivity(this, 0, mapIntent, 0);
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle("Feeling Unmotivated?")
+                        .setContentText("I Gots what ails ya")
+                        .addAction(R.drawable.ic_launcher,
+                                "Hit me!", mapPendingIntent);
+
+// Get an instance of the NotificationManager service
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+
+// Build the notification and issues it with notification manager.
+        notificationManager.notify(notificationId, notificationBuilder.build());
+
+        finish();
     }
 }
